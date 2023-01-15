82223ddf9eda23f2b5fda48749014802f1ff1616
hector <hectorrrivera1010@gmail.com>
2023-01-14 21:39:13 -0600

    initial commit 14.1.2023

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-gsap.from('.img1',{ opacity: 0 })
-gsap.to('.img1',{duration: 3, x: 300, y: 50, opacity: 1, scale:1.1} )
+gsap.registerPlugin(ScrollTrigger)
+
+gsap.from('.img1',{ 
+    opacity: 0 }
+)
+gsap.to('.img1',{
+    duration: 3, 
+    x: 20, 
+    y: -100, 
+    opacity: 1, 
+    scale:1.1} 
+)
+
+gsap.from('.img2',{ 
+    opacity: 0 }
+)
+gsap.to('.img2',{
+    scrollTrigger: '.img2', 
+    duration: 3.5, 
+    x: 40, 
+    y: -200, 
+    opacity: 1
+    } 
+)
