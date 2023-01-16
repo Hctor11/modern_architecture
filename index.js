1d7e28feff493349ddd3b1bf5be6d5ed8c3f446f
hector <hectorrrivera1010@gmail.com>
2023-01-15 21:50:40 -0600

    principal section finished

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 gsap.registerPlugin(ScrollTrigger)
 
+// h1
+gsap.from('.h1',{ 
+    opacity: 0 }
+)
+gsap.to('.h1',{
+    duration: 1, 
+    y: -20, 
+    opacity: 1, } 
+)
+
+// p
+gsap.from('.p',{ 
+    opacity: 0 }
+)
+gsap.to('.p',{
+    duration: 1.5, 
+    y: -20, 
+    opacity: 1, } 
+)
+
+// images -------------------------------------------------------------
 gsap.from('.img1',{ 
     opacity: 0 }
 )
@@ -15,7 +36,6 @@ gsap.from('.img2',{
     opacity: 0 }
 )
 gsap.to('.img2',{
-    scrollTrigger: '.img2', 
     duration: 3.5, 
     x: 40, 
     y: -200, 
